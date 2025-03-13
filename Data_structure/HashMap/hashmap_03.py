7c9a1be81546b6ed27f0ce574ec49ddb65019006
anuj <anujb364@gmail.com>
2025-03-13 18:44:59 +0545

    Add implementation for duplicate count functions in HashMap module

---
+++ Data_structure/HashMap/hashmap_03.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+def duplicate_count(nums:list):
+    count_map = defaultdict(int)
+    for n in nums:
+        count_map[n] +=1
+    for value in count_map.values():
+        if value >= 2:
+            return True
+        else:
+            return False
+        
+def set_duplicate_count(nums:list):
+    hashset = set(nums)
+    for n in nums:
+        if n in hashset:
+            return True
+        else:
+            hashset.add(n)
+    return False
+    
+
+
+if __name__ == "__main__":
+    numbers = [2,14,18,22,22]
+    result = set_duplicate_count(numbers)
+    print(result)
+
