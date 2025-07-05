8c8503c778507882409811fc86edd82cb27ae2e5
anuj <anujb364@gmail.com>
2025-07-05 21:11:11 +0545

    Add binary search implementation and binary tree structure with pre-order traversal

---
+++ Algomap/BInary_search/problem_35.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def searchInsert(self,nums:List[int], target):
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            m = (l + r ) // 2
+
+            if nums[m] < target:
+                l = m + 1
+
+            if nums[m] > target:
+                r = m - 1
+            
+            else: 
+                return m
+        
+        if nums[m] < target:
+            return m + 1
+        
+        else:
+            return m
